d480367a8bbce83d7143de04ebf32c272bc33f52
Manuel Alejandro Suarez <alejosu@gmail.com>
2016-06-03 13:25:01 -0500

    Ajustes a modelos

--- softmas/softmas/app/Empresa.php
+++ softmas/softmas/app/Empresa.php
@@ -4,12 +4,10 @@ namespace softmas;
 
 use Illuminate\Database\Eloquent\Model;
 
-use softmas\Usuario;
-
 class Empresa extends Model
 {
     public function usuarios()
     {
-      return this->hasMany(Usuario::class);
+      return $this->hasMany(Usuario::class);
     }
 }
--- softmas/softmas/app/Usuario.php
+++ softmas/softmas/app/Usuario.php
@@ -4,12 +4,10 @@ namespace softmas;
 
 use Illuminate\Database\Eloquent\Model;
 
-use softmas\Empresa;
-
 class Usuario extends Model
 {
     public function empresa()
     {
-      return this->belongsTo(Empresa::class);
+      return $this->belongsTo(Empresa::class);
     }
 }
